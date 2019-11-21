0c99f96e5293759051326fb83bdaac3d303472f2
q15283013220 <15283013220@qq.com>
2019-11-21 16:33:27 +0800

    lky-Mybatis整合2

---
+++ entity/src/main/java/com/cn/tenmall/entity/WxTabBrand.java
@@ -0,0 +1,12 @@
+package com.cn.tenmall.entity;
+
+import lombok.Data;
+
+@Data
+public class WxTabBrand {
+    private Integer id;
+    private String name;
+    private String image;
+    private String letter;
+    private String seq;
+}
