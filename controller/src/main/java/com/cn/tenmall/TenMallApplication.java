47091d69d3e3d04c6d69e3b3b9e60212575b0b47
Administrator <1260612638@qq.com>
2019-11-21 13:03:39 +0800

    application-default active:pro

--- controller/src/main/java/com/cn/tenmall/TenMallApplication.java
+++ controller/src/main/java/com/cn/tenmall/TenMallApplication.java
@@ -2,15 +2,20 @@ package com.cn.tenmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author Administrator
  */
+@EnableConfigurationProperties
 @SpringBootApplication
 public class TenMallApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TenMallApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(TenMallApplication.class);
+//        禁用命令行更改配置文件属性
+//        springApplication.setAddCommandLineProperties(false);
+        springApplication.run(args);
     }
 
 }
--- controller/src/main/java/com/cn/tenmall/controller/order/OrderController.java
+++ controller/src/main/java/com/cn/tenmall/controller/order/OrderController.java
@@ -1,10 +1,20 @@
 package com.cn.tenmall.controller.order;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author luoyuequan
  * @version 1.00
  * @ClassName OrderController
  * @time 2019/11/21 12:14
  */
+@RestController
+@RequestMapping("/order/*")
 public class OrderController {
+    @RequestMapping(value = "getMsg", method = {RequestMethod.GET})
+    public String getMsg() {
+        return "test";
+    }
 }
