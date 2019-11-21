9ae4d43ce9a44a27fc59bf21621732188ae75cd7
Administrator <1260612638@qq.com>
2019-11-21 09:47:03 +0800

    项目构建

---
+++ controller/src/main/java/com/cn/tenmall/TenMallApplication.java
@@ -0,0 +1,16 @@
+package com.cn.tenmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class TenMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenMallApplication.class, args);
+    }
+
+}
